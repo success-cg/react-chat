8d77a451fc10292ec2f252651bf13a66be3acc30
chengong <654450188@qq.com>
2018-08-05 00:08:51 +0800

    add: react-router-dom制作多页应用

--- src/index.js
+++ src/index.js
@@ -3,6 +3,7 @@ import { createStore, applyMiddleware, compose } from 'redux';
 import ReactDOM from 'react-dom';
 import thunk from 'redux-thunk';
 import { Provider } from 'react-redux';
+import { BrowserRouter as Router, Route, Link } from 'react-router-dom';
 import App from './App.js';
 import './index.css';
 import registerServiceWorker from './registerServiceWorker';
@@ -15,9 +16,23 @@ const store = createStore(reducer, compose(
   window.devToolsExtension ? window.devToolsExtension() : () => {}
 ));
 
+const Eryin = () => (<h1>二营</h1>);
+const Qibinglian = () => (<h1>骑兵连</h1>);
+
 ReactDOM.render(
   <Provider store={ store }>
-    <App/>
+    <Router>
+      <div>
+        <ul>
+          <li><Link to='/'>一营</Link></li>
+          <li><Link to='/erying'>二营</Link></li>
+          <li><Link to='/qibinlian'>骑兵连</Link></li>
+        </ul>
+        <Route path='/' exact component={ App }/>
+        <Route path='/erying' component={ Eryin }/>
+        <Route path='/qibinlian' component={ Qibinglian }/>
+      </div>
+    </Router>
   </Provider>,
   document.getElementById('root'));
 
